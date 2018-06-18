c9f0f18f4faedd7226e73554cdb31a153955f213
Emile Akbarzadeh <emile.aks@gmail.com>
2018-06-18 10:40:00 +1000

    Initial commit

---
+++ GoodOldSerial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace GoodOldSerial
+{
+    internal class Program
+    {
+        public static SerialPort serial;
+
+        private static void Main(string[] args)
+        {
+            basicSetup();
+            while (true)
+            {
+                serial.WriteLine(Console.ReadLine());
+            }
+        }
+
+        private static void basicSetup()
+        {
+            Console.WriteLine("Port?");
+            string port = Console.ReadLine();
+            Console.WriteLine("Baud Rate?");
+            int rate = Convert.ToInt32(Console.ReadLine());
+            serial = new SerialPort(port, rate);
+            serial.Open();
+            serial.DataReceived += dataReceive;
+        }
+
+        private static void dataReceive(object obj, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine(serial.ReadLine());
+        }
+    }
+}
